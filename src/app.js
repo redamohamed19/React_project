f995108ccf4a82d6c6ef37230102d3c83a55279f
Sayah Reda <reda1998.rs@gmail.com>
2021-12-06 11:27:06 +0100

    use Props

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
-const Pet = () => {
+const Pet = props => {
   return React.createElement('div', {}, [
-    React.createElement('h2', {}, 'husky'),
-    React.createElement('h3', {}, 'white'),
-    React.createElement('h3', {}, 'male')
+    React.createElement('h2', {}, props.name),
+    React.createElement('h3', {}, props.color),
+    React.createElement('h3', {}, props.gender)
   ]);
 };
 
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', { id: 'fID' }, 'Adopt me'),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, { name: 'jack', color: 'white', gender: 'male' }),
+    React.createElement(Pet, {
+      name: 'siela',
+      color: 'brown',
+      gender: 'female'
+    }),
+    React.createElement(Pet, { name: 'rock', color: 'black', gender: 'male' })
   ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById('root'));

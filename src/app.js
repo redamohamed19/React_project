c717dd240e9e023fe6640b8f98c35b67f92c575d
Sayah Reda <reda1998.rs@gmail.com>
2021-12-08 00:26:31 +0100

    install prettier

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-const Pet = props => {
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, props.name),
-    React.createElement('h3', {}, props.color),
-    React.createElement('h3', {}, props.gender)
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.color),
+    React.createElement("h3", {}, props.gender),
   ]);
 };
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'fID' }, 'Adopt me'),
-    React.createElement(Pet, { name: 'jack', color: 'white', gender: 'male' }),
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "fID" }, "Adopt me"),
+    React.createElement(Pet, { name: "jack", color: "white", gender: "male" }),
     React.createElement(Pet, {
-      name: 'siela',
-      color: 'brown',
-      gender: 'female'
+      name: "siela",
+      color: "brown",
+      gender: "female",
     }),
-    React.createElement(Pet, { name: 'rock', color: 'black', gender: 'male' })
+    React.createElement(Pet, { name: "rock", color: "black", gender: "male" }),
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));

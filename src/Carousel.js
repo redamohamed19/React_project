192878f73dadcb237936bab404e2a07d2e31c0ae
Sayah Reda <reda1998.rs@gmail.com>
2022-01-29 23:40:46 +0100

    manage state in class component

---
+++ src/Carousel.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+class Carousel extends Component {
+  state = {
+    active: 1
+  };
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"]
+  };
+  handleIndexClick = e => {
+    this.setState({ active: +e.target.dataset.index });
+  };
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default Carousel;
--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 class ClassComponent extends Component {
   //class gonna replace function component
   constructor() {
@@ -30,7 +31,7 @@ class ClassComponent extends Component {
     } = this.state;
     return (
       <div className="details">
-        {" "}
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
